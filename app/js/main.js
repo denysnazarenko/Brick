87ae61ee54eed99237ce3d2d7f9a168bbdbac2cb
denys nazarenko <dinis.nazarenko03@gmail.com>
2024-01-19 13:41:50 +0200

    add js for section main

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Header
   const iconMenu = document.querySelector('.menu__icon');
   if (iconMenu) {
     const menuBody = document.querySelector('.menu__body');
@@ -10,4 +11,47 @@ window.addEventListener('DOMContentLoaded', () => {
       menuBody.classList.toggle('_active');
     });
   }
+
+  // Application Form
+  const basicForm = document.querySelector('.form-main__form'),           
+        basicInputName = document.querySelector('.form-main__name'),
+        basicInputPhone = document.querySelector('.form-main__tel'),
+        basicButtonSubmit = document.querySelector('.form-main__button');
+
+  const userData = {
+      userName: '', 
+      userPhone: ''
+  };
+
+  const emptyInput = function (input) {
+    input.style.cssText = 'border: 1px solid rgb(228, 56, 56); box-shadow: 0px 0px 10px rgb(228, 56, 56)';
+  };
+  const focusInput = function (input) {
+    input.addEventListener('focus', () => {
+      input.style.cssText = 'border: 1px solid rgba(255, 255, 255, 0.70); box-shadow: none';
+    })
+  };
+
+  basicButtonSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (basicInputName.value != '') {
+      userData.userName = basicInputName.value;
+    } else {
+      emptyInput(basicInputName);
+    }
+    if (basicInputPhone.value != '') {
+      userData.userPhone = basicInputPhone.value;
+    } else {
+      emptyInput(basicInputPhone);
+    }
+
+    if (basicInputName.value != '' && basicInputPhone.value != '') {
+      basicForm.reset();
+      console.log(userData);
+    }
+
+    focusInput(basicInputName);
+    focusInput(basicInputPhone);
+  });
+
 });
